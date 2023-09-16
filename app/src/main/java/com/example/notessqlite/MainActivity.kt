119a1b1837372b98904d5a1e24d71ab221e5a23e
sasinduSilva <sinharaavishka@gmail.com>
2023-09-17 04:17:50 +0530

    card for recycler view designed

--- app/src/main/java/com/example/notessqlite/MainActivity.kt
+++ app/src/main/java/com/example/notessqlite/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.notessqlite
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.notessqlite.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.addButton.setOnClickListener{
+            val intent = Intent(this,AddNoteActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/notessqlite/Note.kt
@@ -0,0 +1,10 @@
+package com.example.notessqlite
+
+data class Note(
+
+    val id: Int,
+    val title: String,
+    val content: String
+
+
+)
