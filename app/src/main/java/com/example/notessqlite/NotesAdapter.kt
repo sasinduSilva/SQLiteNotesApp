414efb4cec042fc660c75d5174434b44dcc384d8
sasinduSilva <sinharaavishka@gmail.com>
2023-09-17 09:15:06 +0530

    app complete

--- app/src/main/java/com/example/notessqlite/NotesAdapter.kt
+++ app/src/main/java/com/example/notessqlite/NotesAdapter.kt
@@ -7,15 +7,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class NotesAdapter(private var notes: List<Note>, context: Context) :
     RecyclerView.Adapter<NotesAdapter.NoteViewHolder>() {
 
+    private val db: NoteDatabaseHelper = NoteDatabaseHelper(context)
     class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         val titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
         val contentTextView: TextView = itemView.findViewById(R.id.contentTextView)
         val updateButton: ImageView = itemView.findViewById(R.id.updateButton)
+        val deleteButton: ImageView = itemView.findViewById(R.id.deleteButton)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
@@ -33,10 +36,17 @@ class NotesAdapter(private var notes: List<Note>, context: Context) :
         holder.updateButton.setOnClickListener{
             val intent = Intent(holder.itemView.context, AddNoteActivity::class.java).apply {
                 putExtra("note_id", note.id)
-                putExtra("isUpdate", true)
+
             }
             holder.itemView.context.startActivity(intent)
         }
+
+        holder.deleteButton.setOnClickListener {
+            db.deleteNote(note.id)
+            refreshData(db.getAllNotes())
+            Toast.makeText(holder.itemView.context,"Note Deleted",Toast.LENGTH_SHORT).show()
+        }
+
     }
 
     fun refreshData(newNotes: List<Note>){
