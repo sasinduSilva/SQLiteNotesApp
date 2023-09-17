64c2e7a0eb10f43998bb650cd9bec430a45de236
sasinduSilva <sinharaavishka@gmail.com>
2023-09-17 08:32:32 +0530

    update done

--- app/src/main/java/com/example/notessqlite/AddNoteActivity.kt
+++ app/src/main/java/com/example/notessqlite/AddNoteActivity.kt
@@ -9,20 +9,42 @@ class AddNoteActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAddNoteBinding
     private lateinit var db: NoteDatabaseHelper
+    private  var noteId : Int = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddNoteBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         db = NoteDatabaseHelper(this)
+        noteId = intent.getIntExtra("note_id",-1)
+        if(noteId != -1){
+            val updatingObj = db.getNoteById(noteId)
+            binding.titleEditText.setText(updatingObj.title)
+            binding.contentEditText.setText(updatingObj.content)
+            binding.addNoteHeading.setText("Update Note")
+        }
+
+
 
         binding.saveButton.setOnClickListener{
             val title = binding.titleEditText.text.toString()
             val content = binding.contentEditText.text.toString()
-            val note = Note(0,title,content)
-            db.insertNote(note)
-            finish()
-            Toast.makeText(this,"Note Saved", Toast.LENGTH_SHORT).show()
+
+
+            if(noteId == -1){
+                val note = Note(0,title,content)
+                db.insertNote(note)
+                finish()
+                Toast.makeText(this,"Note Saved", Toast.LENGTH_SHORT).show()
+            }else{
+                val updateNote = Note(noteId,title,content)
+                db.updateNote(updateNote)
+                finish()
+                Toast.makeText(this,"Note Updated", Toast.LENGTH_SHORT).show()
+            }
+
+
         }
     }
 }
